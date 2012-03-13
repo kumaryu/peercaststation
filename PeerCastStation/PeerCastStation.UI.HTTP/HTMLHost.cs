9c9c022ba9b201027ebee31b6078ef644fa185ce
kumaryu <kumaryu@kumaryu.net>
2012-03-13 23:00:15 +0000

    HTMLHostでHTML等にテンプレート適用処理を行なって出力していたところを使ってないので外した。

--- PeerCastStation/PeerCastStation.UI.HTTP/HTMLHost.cs
+++ PeerCastStation/PeerCastStation.UI.HTTP/HTMLHost.cs
@@ -8,147 +8,6 @@ using System.Collections.Generic;
 
 namespace PeerCastStation.UI.HTTP
 {
-  public static class UriEscapeFilter
-  {
-    public static string UriEscape(string input)
-    {
-      return Uri.IsWellFormedUriString(input, UriKind.RelativeOrAbsolute) ? input : Uri.EscapeUriString(input);
-    }
-
-    public static string U(string input)
-    {
-      return UriEscape(input);
-    }
-  }
-
-  public class ChannelTrackController
-    : DotLiquid.ILiquidizable
-  {
-    public ChannelTrack Track { get; private set; }
-    public ChannelTrackController(ChannelTrack track)
-    {
-      this.Track = track;
-    }
-
-    public object ToLiquid()
-    {
-      return new {
-        name    = Track.Name,
-        album   = Track.Album,
-        creator = Track.Creator,
-        url     = Track.URL,
-        genre   = "",
-      };
-    }
-  }
-
-  public class ChannelInfoController
-    : DotLiquid.ILiquidizable
-  {
-    public ChannelInfo Info { get; private set; }
-    public ChannelInfoController(ChannelInfo info)
-    {
-      this.Info = info;
-    }
-
-    public object ToLiquid()
-    {
-      return new {
-        name         = Info.Name,
-        bitrate      = Info.Bitrate,
-        comment      = Info.Comment,
-        content_type = Info.ContentType,
-        mime_type    = Info.MIMEType,
-        description  = Info.Desc,
-        genre        = Info.Genre,
-        url          = Info.URL,
-      };
-    }
-  }
-
-  public class OutputStreamController
-    : DotLiquid.ILiquidizable
-  {
-    public IOutputStream Stream { get; set; }
-    public OutputStreamController(IOutputStream stream)
-    {
-      this.Stream = stream;
-    }
-
-    public object ToLiquid()
-    {
-      return new {
-        is_local      = Stream.IsLocal,
-        type          = Stream.OutputStreamType,
-        upstream_rate = Stream.UpstreamRate,
-        description   = Stream.ToString(),
-      };
-    }
-  }
-
-  public class ChannelController
-    : DotLiquid.ILiquidizable
-  {
-    public Channel Channel { get; private set; }
-    public ChannelController(Channel channel)
-    {
-      this.Channel = channel;
-    }
-
-    public object ToLiquid()
-    {
-      return new {
-        id    = Channel.ChannelID.ToString("N").ToUpper(),
-        info  = new ChannelInfoController(Channel.ChannelInfo),
-        track = new ChannelTrackController(Channel.ChannelTrack),
-        playlist_url     = "/pls/" + Channel.ChannelID.ToString("N").ToUpper(),
-        stream_url       = "/stream/" + Channel.ChannelID.ToString("N").ToUpper(),
-        content_position = Channel.ContentPosition,
-        header_position  = Channel.ContentHeader!=null ? Channel.ContentHeader.Position : -1,
-        header_length    = Channel.ContentHeader!=null ? Channel.ContentHeader.Data.Length : 0,
-        local_directs    = Channel.LocalDirects,
-        local_relays     = Channel.LocalRelays,
-        total_directs    = Channel.TotalDirects,
-        total_relays     = Channel.TotalRelays,
-        uptime           = Channel.Uptime.TotalSeconds,
-        source_uri       = Channel.SourceUri.ToString(),
-        status           = Channel.Status.ToString(),
-        is_relay_full    = Channel.IsRelayFull,
-        is_direct_full   = Channel.IsDirectFull,
-        output_streams   = Channel.OutputStreams.Select(s => new OutputStreamController(s)),
-      };
-    }
-  }
-
-  public class PeerCastController
-    : DotLiquid.ILiquidizable
-  {
-    public PeerCast PeerCast { get; private set; }
-    public PeerCastController(PeerCast peercast)
-    {
-      this.PeerCast = peercast;
-    }
-
-    public object ToLiquid()
-    {
-      return new {
-        channels        = PeerCast.Channels.Select(c => new ChannelController(c)),
-        agent_name      = PeerCast.AgentName,
-        bcid            = PeerCast.BroadcastID.ToString("N").ToUpper(),
-        gloal_endpoint  = PeerCast.GetGlobalEndPoint(System.Net.Sockets.AddressFamily.InterNetwork, OutputStreamType.Relay),
-        local_endpoint  = PeerCast.GetLocalEndPoint(System.Net.Sockets.AddressFamily.InterNetwork, OutputStreamType.Relay),
-        gloal_endpoint6 = PeerCast.GetGlobalEndPoint(System.Net.Sockets.AddressFamily.InterNetworkV6, OutputStreamType.Relay),
-        local_endpoint6 = PeerCast.GetLocalEndPoint(System.Net.Sockets.AddressFamily.InterNetworkV6, OutputStreamType.Relay),
-        is_firewalled   = PeerCast.IsFirewalled,
-        local_directs   = PeerCast.Channels.Sum(c => c.LocalDirects),
-        local_relays    = PeerCast.Channels.Sum(c => c.LocalRelays),
-        upstream_rate   = PeerCast.Channels.Sum(c => c.OutputStreams.Sum(s => s.UpstreamRate)),
-        downstream_rate = PeerCast.Channels.Sum(c => c.Status==SourceStreamStatus.Receiving ? c.ChannelInfo.Bitrate : 0),
-        session_id      = PeerCast.SessionID.ToString("N").ToUpper(),
-      };
-    }
-  }
-
   public class HTMLHost
     : MarshalByRefObject,
       IUserInterface
@@ -156,24 +15,23 @@ namespace PeerCastStation.UI.HTTP
     class FileDesc
     {
       public string MimeType   { get; set; }
-      public bool   IsTemplate { get; set; }
     }
     private static readonly Dictionary<string, FileDesc> FileDescriptions = new Dictionary<string, FileDesc> {
-      { ".html", new FileDesc { MimeType="text/html", IsTemplate=true } },
-      { ".htm",  new FileDesc { MimeType="text/html", IsTemplate=true } },
-      { ".txt",  new FileDesc { MimeType="text/plain", IsTemplate=true } },
-      { ".xml",  new FileDesc { MimeType="text/xml", IsTemplate=true } },
-      { ".json", new FileDesc { MimeType="application/json", IsTemplate=true } },
-      { ".css",  new FileDesc { MimeType="text/css", IsTemplate=false } },
-      { ".js",   new FileDesc { MimeType="application/javascript", IsTemplate=false } },
-      { ".bmp",  new FileDesc { MimeType="image/bmp", IsTemplate=false } },
-      { ".png",  new FileDesc { MimeType="image/png", IsTemplate=false } },
-      { ".jpg",  new FileDesc { MimeType="image/jpeg", IsTemplate=false } },
-      { ".gif",  new FileDesc { MimeType="image/gif", IsTemplate=false } },
-      { ".svg",  new FileDesc { MimeType="image/svg+xml", IsTemplate=false } },
-      { ".swf",  new FileDesc { MimeType="application/x-shockwave-flash", IsTemplate=false } },
-      { ".xap",  new FileDesc { MimeType="application/x-silverlight-app", IsTemplate=false } },
-      { "",      new FileDesc { MimeType="application/octet-stream", IsTemplate=false } },
+      { ".html", new FileDesc { MimeType="text/html" } },
+      { ".htm",  new FileDesc { MimeType="text/html" } },
+      { ".txt",  new FileDesc { MimeType="text/plain" } },
+      { ".xml",  new FileDesc { MimeType="text/xml" } },
+      { ".json", new FileDesc { MimeType="application/json" } },
+      { ".css",  new FileDesc { MimeType="text/css" } },
+      { ".js",   new FileDesc { MimeType="application/javascript" } },
+      { ".bmp",  new FileDesc { MimeType="image/bmp" } },
+      { ".png",  new FileDesc { MimeType="image/png" } },
+      { ".jpg",  new FileDesc { MimeType="image/jpeg" } },
+      { ".gif",  new FileDesc { MimeType="image/gif" } },
+      { ".svg",  new FileDesc { MimeType="image/svg+xml" } },
+      { ".swf",  new FileDesc { MimeType="application/x-shockwave-flash" } },
+      { ".xap",  new FileDesc { MimeType="application/x-silverlight-app" } },
+      { "",      new FileDesc { MimeType="application/octet-stream" } },
     };
 
     public string Name { get { return "HTTP HTML Host UI"; } }
@@ -312,25 +170,7 @@ namespace PeerCastStation.UI.HTTP
             };
             Send(HTTPUtils.CreateResponseHeader(HttpStatusCode.OK, parameters));
             if (this.request.Method=="GET") {
-              if (content_desc.IsTemplate) {
-                DotLiquid.Template.RegisterFilter(typeof(UriEscapeFilter));
-                var template = DotLiquid.Template.Parse(System.Text.Encoding.UTF8.GetString(contents));
-                var tmpparams = new DotLiquid.Hash();
-                var query = ParseQuery(this.request.Uri.Query);
-                if (query.ContainsKey("id")) {
-                  var id = new Guid(query["id"]);
-                  var channel = PeerCast.Channels.FirstOrDefault(c => c.ChannelID==id);
-                  if (channel!=null) {
-                    tmpparams.Add("channel", new ChannelController(channel));
-                  }
-                }
-                tmpparams.Add("peercast", new PeerCastController(PeerCast));
-                var result = template.Render(tmpparams);
-                Send(System.Text.Encoding.UTF8.GetBytes(result));
-              }
-              else {
-                Send(contents);
-              }
+              Send(contents);
             }
           }
           else {
