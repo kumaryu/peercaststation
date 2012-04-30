a2c8e48bbe3162f02ddf2990329ea3911e3e1fa1
kumaryu <kumaryu@kumaryu.net>
2012-04-30 12:57:38 +0000

    各アセンブリのファイルバージョンを0.7.0.0に揃えた。

--- PeerCastStation/PeerCastStation/Properties/AssemblyInfo.cs
+++ PeerCastStation/PeerCastStation/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("PeerCastStation")]
-[assembly: AssemblyCopyright("Copyright ©  2011")]
+[assembly: AssemblyCopyright("Copyright © arekuma 2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -33,4 +33,4 @@ using System.Runtime.InteropServices;
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("0.7.0.0")]
