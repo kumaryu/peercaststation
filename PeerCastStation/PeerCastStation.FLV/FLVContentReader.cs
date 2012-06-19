5a7de5cea383780aef73c1737e47f5bf66511509
kumaryu <kumaryu@kumaryu.net>
2012-06-19 16:07:22 +0000

    FLVを読み込むためのPeerCastStation.FLVを追加した。

---
+++ PeerCastStation/PeerCastStation.FLV/FLVContentReader.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+using PeerCastStation.Core;
+using System.Collections.Generic;
+
+namespace PeerCastStation.FLV
+{
+  internal class BadDataException : ApplicationException
+  {
+  }
+
+  public class FLVContentReader
+    : IContentReader
+  {
+    public FLVContentReader(Channel channel)
+    {
+      this.Channel = channel;
+    }
+
+    public string Name { get { return "Flash Video (FLV)"; } }
+    public Channel Channel { get; private set; }
+
+    private class FileHeader
+    {
+      public byte[] Binary     { get; private set; }
+      public byte[] Signature  { get; private set; }
+      public int    Version    { get; private set; }
+      public bool   HasAudio   { get; private set; }
+      public bool   HasVideo   { get; private set; }
+      public long   DataOffset { get; private set; }
+      public long   Size       { get; private set; }
+
+      public FileHeader(byte[] binary)
+      {
+        this.Binary     = binary;
+        this.Signature  = new byte[] { binary[0], binary[1], binary[2] };
+        this.Version    = binary[3];
+        this.HasAudio   = (binary[4] & 0x4)!=0;
+        this.HasVideo   = (binary[4] & 0x1)!=0;
+        this.DataOffset = (binary[5]<<24) | (binary[ 6]<<16) | (binary[ 7]<<8) | (binary[ 8]<<0);
+        this.Size       = (binary[9]<<24) | (binary[10]<<16) | (binary[11]<<8) | (binary[12]<<0);
+      }
+
+      public bool IsValid {
+        get {
+          return Signature[0]=='F' && Signature[1]=='L' && Signature[2]=='V' && Version==1 && Size==0;
+        }
+      }
+    }
+
+    private class FLVTag
+    {
+      public byte[] Header    { get; private set; }
+      public byte[] Body      { get; private set; }
+      public byte[] Footer    { get; private set; }
+      public bool   Filter    { get; private set; }
+      public int    Type      { get; private set; }
+      public int    DataSize  { get; private set; }
+      public long   Timestamp { get; private set; }
+      public int    StreamID  { get; private set; }
+      public long   TagSize   {
+        get { return FLVContentReader.GetUInt32(this.Footer); }
+      }
+      public byte[] Binary {
+        get { return Header.Concat(Body).Concat(Footer).ToArray(); }
+      }
+
+      public FLVTag(byte[] binary)
+      {
+        this.Header    = binary;
+        this.Filter    = (binary[0] & 0x20)!=0;
+        this.Type      = binary[0] & 0x1F;
+        this.DataSize  =                   (binary[1]<<16) | (binary[2]<<8) | (binary[3]);
+        this.Timestamp = (binary[7]<<24) | (binary[4]<<16) | (binary[5]<<8) | (binary[6]);
+        this.StreamID  =                   (binary[8]<<16) | (binary[9]<<8) | (binary[10]);
+        this.Body      = null;
+        this.Footer    = null;
+      }
+
+      public bool IsValidHeader {
+        get {
+          return
+            (Header[0] & 0xC0)==0 &&
+            (Type==8 || Type==9 || Type==19) &&
+            StreamID==0;
+        }
+      }
+
+      public bool IsValidFooter {
+        get { return this.DataSize+11==this.TagSize; }
+      }
+
+      public void ReadBody(Stream stream)
+      {
+        this.Body = FLVContentReader.ReadBytes(stream, this.DataSize);
+      }
+
+      public void ReadFooter(Stream stream)
+      {
+        this.Footer = FLVContentReader.ReadBytes(stream, 4);
+      }
+    }
+
+    private class TagDesc
+    {
+      public double Timestamp { get; set; }
+      public int    DataSize  { get; set; }
+    }
+
+    private static byte[] ReadBytes(Stream stream, int len)
+    {
+      var res = new byte[len];
+      if (stream.Read(res, 0, len)<len) {
+        throw new EndOfStreamException();
+      }
+      return res;
+    }
+
+    private static long GetUInt32(byte[] bin)
+    {
+      return (bin[0]<<24) | (bin[1]<<16) | (bin[2]<<8) | (bin[3]<<0);
+    }
+
+    private enum ReaderState {
+      Header,
+      Body,
+    };
+    private ReaderState state = ReaderState.Header;
+    private long position = 0;
+    private FileHeader fileHeader;
+    private LinkedList<TagDesc> tags = new LinkedList<TagDesc>();
+
+    public ParsedContent Read(Stream stream)
+    {
+      var res = new ParsedContent();
+      var info = new AtomCollection(Channel.ChannelInfo.Extra);
+      var processed = false;
+      var eos = false;
+      while (!eos) {
+        var start_pos = stream.Position;
+        try {
+          switch (state) {
+          case ReaderState.Header:
+            {
+              var bin = ReadBytes(stream, 13);
+              var header = new FileHeader(bin);
+              if (header.IsValid) {
+                fileHeader = header;
+                bin = header.Binary;
+                res.ContentHeader = new Content(position, bin);
+                res.Contents = null;
+                info.SetChanInfoType("FLV");
+                info.SetChanInfoStreamType("video/x-flv");
+                info.SetChanInfoStreamExt(".flv");
+                res.ChannelInfo = new ChannelInfo(info);
+                position = bin.Length;
+                tags.Clear();
+                state = ReaderState.Body;
+              }
+              else {
+                throw new BadDataException();
+              }
+            }
+            break;
+          case ReaderState.Body:
+            {
+              var bin = ReadBytes(stream, 11);
+              var read_valid = false;
+              var body = new FLVTag(bin);
+              if (body.IsValidHeader) {
+                body.ReadBody(stream);
+                body.ReadFooter(stream);
+                if (body.IsValidFooter) {
+                  read_valid = true;
+                  bin = body.Binary;
+                  if (res.Contents==null) res.Contents = new List<Content>();
+                  res.Contents.Add(new Content(position, bin));
+                  tags.AddLast(new TagDesc { Timestamp=body.Timestamp/1000.0, DataSize=body.DataSize });
+                  var timespan = tags.Last.Value.Timestamp-tags.First.Value.Timestamp;
+                  if (timespan>=30.0) {
+                    var sz = tags.Take(tags.Count-1).Sum(t => t.DataSize);
+                    info.SetChanInfoBitrate((int)(sz*8/timespan+900)/1000);
+                    res.ChannelInfo = new ChannelInfo(info);
+                    while (tags.Count>1) tags.RemoveFirst();
+                  }
+                  position += bin.Length;
+                }
+              }
+              if (!read_valid) {
+                stream.Position = start_pos;
+                var header = new FileHeader(ReadBytes(stream, 13));
+                if (header.IsValid) {
+                  read_valid = true;
+                  fileHeader = header;
+                  bin = header.Binary;
+                  res.ContentHeader = new Content(0, bin);
+                  res.Contents = null;
+                  info.SetChanInfoType("FLV");
+                  info.SetChanInfoStreamType("video/x-flv");
+                  info.SetChanInfoStreamExt(".flv");
+                  res.ChannelInfo = new ChannelInfo(info);
+                  tags.Clear();
+                  position = bin.Length;
+                }
+              }
+              if (!read_valid) throw new BadDataException();
+            }
+            break;
+          }
+          processed = true;
+        }
+        catch (EndOfStreamException) {
+          if (!processed) throw;
+          stream.Position = start_pos;
+          eos = true;
+        }
+        catch (BadDataException) {
+          stream.Position = start_pos+1;
+        }
+      }
+      return res;
+    }
+  }
+
+  [Plugin]
+  public class FLVContentReaderFactory
+    : IContentReaderFactory
+  {
+    public string Name { get { return "Flash Video (FLV)"; } }
+
+    public IContentReader Create(Channel channel)
+    {
+      return new FLVContentReader(channel);
+    }
+  }
+}
---
+++ PeerCastStation/TestFLV/test_flvcontentreader.rb
@@ -0,0 +1,290 @@
+﻿# coding:utf-8
+# PeerCastStation, a P2P streaming servent.
+# Copyright (C) 2012 Ryuichi Sakamoto (kumaryu@kumaryu.net)
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+$: << File.join(File.dirname(__FILE__), '..', 'PeerCastStation.Core', 'bin', 'Debug')
+$: << File.join(File.dirname(__FILE__), '..', 'PeerCastStation.FLV', 'bin', 'Debug')
+require 'PeerCastStation.Core.dll'
+require 'PeerCastStation.FLV.dll'
+require 'test/unit'
+require 'shoulda/context'
+PCSCore = PeerCastStation::Core unless defined?(PCSCore)
+PCSFLV  = PeerCastStation::FLV unless defined?(PCSFLV)
+
+class TC_FLVContentReaderFactory < Test::Unit::TestCase
+  context 'コンストラクタ' do
+    setup do
+      @factory = PCSFLV::FLVContentReaderFactory.new
+    end
+
+    should 'IContentReaderFactoryを実装している' do
+      assert_kind_of PCSCore::IContentReaderFactory, @factory
+    end
+
+    should 'NameがFLVっぽいのを返す' do
+      assert_equal "Flash Video (FLV)", @factory.name
+    end
+  end
+
+  context 'Create' do
+    setup do
+      @factory = PCSFLV::FLVContentReaderFactory.new
+      @peercast = PCSCore::PeerCast.new
+      @channel = PCSCore::Channel.new(
+        @peercast,
+        System::Guid.new('9778E62BDC59DF56F9216D0387F80BF2'.to_clr_string), 
+        System::Uri.new('http://127.0.0.1:8888/'))
+    end
+    
+    teardown do
+      @peercast.stop if @peercast
+    end
+
+    should 'IContentReaderを実装したインスタンスを返す' do
+      reader = @factory.create(@channel)
+      assert_kind_of PCSCore::IContentReader, reader
+    end
+
+    should 'NameがFLVっぽいのを返す' do
+      reader = @factory.create(@channel)
+      assert_equal "Flash Video (FLV)", reader.name
+    end
+  end
+end
+
+class TC_FLVContentReader < Test::Unit::TestCase
+  def fixture(name)
+    File.join(File.dirname(__FILE__), 'fixtures', name)
+  end
+
+  context 'Read' do
+    setup do
+      @peercast = PCSCore::PeerCast.new
+      @channel = PCSCore::Channel.new(
+        @peercast,
+        System::Guid.new('9778E62BDC59DF56F9216D0387F80BF2'.to_clr_string), 
+        System::Uri.new('http://127.0.0.1:8888/'))
+      @factory = PCSFLV::FLVContentReaderFactory.new
+      @reader = @factory.create(@channel)
+    end
+    
+    teardown do
+      @peercast.stop if @peercast
+    end
+
+    should '空のストリームから読むとEndOfStreamExceptionを投げる' do
+      stream = System::IO::MemoryStream.new
+      assert_raises System::IO::EndOfStreamException do
+        content = @reader.read(stream)
+      end
+    end
+
+    context 'test.flv' do
+      setup do
+        @stream = System::IO::File.open_read(fixture('test.flv'))
+      end
+
+      teardown do
+        @stream.close if @stream
+      end
+
+      should '例外を投げずに読み取れる' do
+        assert_nothing_raised do
+          contents = @reader.read(@stream)
+        end
+      end
+
+      should '一度で出来る限り読み込み、二度目の読み込みはEndOfStreamException' do
+        contents = @reader.read(@stream)
+        assert_raises System::IO::EndOfStreamException do
+          contents = @reader.read(@stream)
+        end
+      end
+
+      should 'null以外を返す' do
+        assert_not_nil @reader.read(@stream)
+      end
+
+      should 'ContentHeaderがnullでなく、Positionは0' do
+        res = @reader.read(@stream)
+        assert_not_nil res.ContentHeader
+        assert_equal 0, res.ContentHeader.Position
+      end
+
+      should 'ChannelInfoに値が入っている' do
+        res = @reader.read(@stream)
+        assert_not_nil res.ChannelInfo
+        assert_equal 'video/x-flv', res.ChannelInfo.MIMEType
+        assert_equal '.flv', res.ChannelInfo.ContentExtension
+        assert_equal 'FLV',  res.ChannelInfo.ContentType
+        assert res.ChannelInfo.Bitrate!=0
+      end
+
+      should 'Contentsにいくらかのパケットが入っている' do
+        res = @reader.read(@stream)
+        assert_not_nil res.Contents
+        assert 0<res.Contents.count
+      end
+
+      should 'ContentHeaderとContentsのパケットは連続している' do
+        res = @reader.read(@stream)
+        pos  = res.ContentHeader.Position
+        pos += res.ContentHeader.Data.length
+        res.Contents.each do |content|
+          assert_equal content.Position, pos
+          pos += content.Data.length
+        end
+      end
+    end
+  end
+
+  context '少しづつRead' do
+    setup do
+      @peercast = PCSCore::PeerCast.new
+      @channel = PCSCore::Channel.new(
+        @peercast,
+        System::Guid.new('9778E62BDC59DF56F9216D0387F80BF2'.to_clr_string), 
+        System::Uri.new('http://127.0.0.1:8888/'))
+      @factory = PCSFLV::FLVContentReaderFactory.new
+      @reader = @factory.create(@channel)
+      @stream = System::IO::MemoryStream.new
+    end
+    
+    teardown do
+      @stream.close if @stream
+      @peercast.stop if @peercast
+    end
+
+    def copy_to(dst, src, len)
+      bytes = System::Array[System::Byte].new(len)
+      len = src.Read(bytes, 0, len)
+      dst.Write(bytes, 0, len) if len>0
+      len
+    end
+
+    def read_slow(&block)
+      pos = @stream.position
+      count = 1024
+      begin
+        res = nil
+        loop do
+          @stream.position = pos
+          res = @reader.read(@stream)
+          if res or
+             res.ContentHeader.nil? and
+             res.Contents.nil? and
+             res.ChannelInfo.nil? then
+            pos = @stream.position
+            count = 1024
+          else
+            break
+          end
+        end
+        block.call(res) if block
+        pos = @stream.position
+        count = 1024
+      rescue System::IO::EndOfStreamException
+        @stream.position = @stream.length
+        if copy_to(@stream, @file, count)>0 then
+          count = [count*2, 65536].min
+          @stream.position = pos
+          retry
+        end
+      end
+      res
+    end
+
+    context 'test.flv' do
+      setup do
+        @file = System::IO::File.open_read(fixture('test.flv'))
+      end
+
+      teardown do
+        @file.close if @file
+      end
+
+      should '読んだだけで足りなければEndOfStreamException' do
+        copy_to(@stream, @file, 8)
+        @stream.position = 0
+        assert_raises System::IO::EndOfStreamException do
+          contents = @reader.read(@stream)
+        end
+      end
+
+      should '少しずつ読んでいってContentHeaderが読み込める' do
+        header = nil
+        read_slow do |res|
+          assert_not_nil res.ContentHeader
+          assert_equal 0, res.ContentHeader.Position
+        end
+      end
+
+      should 'ヘッダを読み込んだあとも少しづつ読んでいくとContentが読み込める' do
+        header = nil
+        contents = []
+        until header do 
+          read_slow do |res|
+            if res.ContentHeader then
+              header = res.ContentHeader
+            end
+            if res.Contents then
+              res.Contents.each do |content|
+                contents << content
+              end
+            end
+          end
+        end
+
+        while @file.position<@file.length do 
+          read_slow do |res|
+            res.Contents.each do |content|
+              contents << content
+            end
+          end
+        end
+        pos  = header.Position
+        pos += header.Data.length
+        contents.each do |content|
+          assert_equal pos, content.Position
+          pos += content.Data.length
+        end
+      end
+
+      should 'ContentHeaderまで読んだ時点でChannelInfoに値が入る' do
+        res = read_slow 
+        assert_not_nil res.ChannelInfo
+        assert_equal 'video/x-flv', res.ChannelInfo.MIMEType
+        assert_equal '.flv', res.ChannelInfo.ContentExtension
+        assert_equal 'FLV',  res.ChannelInfo.ContentType
+        assert_equal 0,      res.ChannelInfo.Bitrate
+      end
+
+      should '少しづつ読んでいくとChannelInfo.Bitrateが更新される' do
+        header = read_slow
+
+        infos = []
+        while @file.position<@file.length do 
+          read_slow do |res|
+            infos << res.ChannelInfo if res.ChannelInfo
+          end
+        end
+        assert_not_empty infos
+        infos.each do |info|
+          assert_not_equal 0, info.Bitrate
+        end
+      end
+    end
+  end
+end
