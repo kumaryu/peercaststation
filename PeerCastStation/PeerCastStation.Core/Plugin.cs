45238e34eb9106b7b31617b36c284ce436e8c13e
kumaryu <kumaryu@kumaryu.net>
2013-04-04 00:33:03 +0900

    プラグイン関係のインターフェース定義部分を別ファイルに分割した

---
+++ PeerCastStation/PeerCastStation.Core/Plugin.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace PeerCastStation.Core
+{
+  public enum PluginPriority
+  {
+    Highest = -200,
+    Higher  = -100,
+    Normal  =    0,
+    Lower   =  100,
+    Lowest  =  200,
+  }
+
+  [AttributeUsage(AttributeTargets.Class)]
+  public class PluginAttribute
+    : Attribute
+  {
+    public PluginPriority Priority { get; private set; }
+
+    public PluginAttribute(PluginPriority priority)
+    {
+      this.Priority = priority;
+    }
+
+    public PluginAttribute()
+      : this(PluginPriority.Normal)
+    {
+    }
+  }
+
+  public interface IPlugin
+  {
+    string Name   { get; }
+    bool IsUsable { get; }
+    void Start(PeerCastApplication app);
+    void Stop();
+  }
+}
