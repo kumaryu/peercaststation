603c1fa76a55fcb2864ab74129ae2f00be6f4f04
kumaryu <kumaryu@kumaryu.net>
2013-05-03 23:18:49 +0900

    Bumped version up to 1.3.0

--- PeerCastStation/PeerCastStation.WPF/Properties/AssemblyInfo.cs
+++ PeerCastStation/PeerCastStation.WPF/Properties/AssemblyInfo.cs
@@ -67,4 +67,4 @@ using System.Windows;
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.2.1.0")]
+[assembly: AssemblyFileVersion("1.3.0.0")]
