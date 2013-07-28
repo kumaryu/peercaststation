d267220f254c83e18a40711eeaa669a3c274bfce
kumaryu <kumaryu@kumaryu.net>
2013-07-28 13:14:19 +0900

    Channelのメンバ等を整理した
    Channel.SourceStreamがなぜか設定可能になっていたが、Channel.Startで設定するのでプロパティでは設定はできないようにした。
    OutputStreamCollectionはいらなかったのでListに置き換えた。
    Channel.csに入っていたChannelInfoとChannelTrackをChannelInfo.csに分割した。

--- PeerCastStation/PeerCastStation.Core/Channel.cs
+++ PeerCastStation/PeerCastStation.Core/Channel.cs
@@ -21,212 +21,6 @@ using System.Net.Sockets;
 
 namespace PeerCastStation.Core
 {
-  /// <summary>
-  /// チャンネルのメタデータを保持するクラスです
-  /// </summary>
-  public class ChannelInfo
-  {
-    /// <summary>
-    /// チャンネル名を取得します
-    /// </summary>
-    public string Name {
-      get {
-        return extra.GetChanInfoName();
-      }
-    }
-
-    /// <summary>
-    /// チャンネルストリームの内容種類を取得します
-    /// </summary>
-    public string ContentType {
-      get {
-        return extra.GetChanInfoType();
-      }
-    }
-
-    /// <summary>
-    /// ジャンルを取得します
-    /// </summary>
-    public string Genre {
-      get {
-        return extra.GetChanInfoGenre();
-      }
-    }
-
-    /// <summary>
-    /// チャンネル詳細を取得します
-    /// </summary>
-    public string Desc {
-      get {
-        return extra.GetChanInfoDesc();
-      }
-    }
-
-    /// <summary>
-    /// 配信コメントを取得します
-    /// </summary>
-    public string Comment {
-      get {
-        return extra.GetChanInfoComment();
-      }
-    }
-
-    /// <summary>
-    /// コンタクトURLを取得します
-    /// </summary>
-    public string URL {
-      get {
-        return extra.GetChanInfoURL();
-      }
-    }
-
-    /// <summary>
-    /// 配信ビットレート情報を取得します
-    /// </summary>
-    public int Bitrate
-    {
-      get
-      {
-        return extra.GetChanInfoBitrate() ?? 0;
-      }
-    }
-
-    /// <summary>
-    /// ストリームのMIME Typeを取得します。
-    /// </summary>
-    public string MIMEType {
-      get {
-        var stream_type = extra.GetChanInfoStreamType();
-        if (!String.IsNullOrEmpty(stream_type)) {
-          return stream_type;
-        }
-        else {
-          switch (ContentType) {
-          case "MP3": return "audio/mpeg";
-          case "OGG": return "audio/ogg";
-          case "OGM": return "video/ogg";
-          case "RAW": return "application/octet-stream";
-          case "NSV": return "video/nsv";
-          case "WMA": return "audio/x-ms-wma";
-          case "WMV": return "video/x-ms-wmv";
-          case "PLS": return "audio/mpegurl";
-          case "M3U": return "audio/m3u";
-          case "ASX": return "video/x-ms-asf";
-          default: return "application/octet-stream";
-          }
-        }
-      }
-    }
-
-    /// <summary>
-    /// ストリームファイルの拡張子を取得します
-    /// </summary>
-    public string ContentExtension
-    {
-      get {
-        var stream_ext = extra.GetChanInfoStreamExt();
-        if (!String.IsNullOrEmpty(stream_ext)) {
-          return stream_ext;
-        }
-        else {
-          switch (ContentType) {
-          case "MP3": return ".mp3";
-          case "OGG": return ".ogg";
-          case "OGM": return ".ogv";
-          case "RAW": return "";
-          case "NSV": return ".nsv";
-          case "WMA": return ".wma";
-          case "WMV": return ".wmv";
-          case "PLS": return ".pls";
-          case "M3U": return ".m3u";
-          case "ASX": return ".asx";
-          default: return "";
-          }
-        }
-      }
-    }
-
-    private ReadOnlyAtomCollection extra;
-    /// <summary>
-    /// その他のチャンネル情報を保持するリストを取得します
-    /// </summary>
-    public IAtomCollection Extra { get { return extra; } }
-
-    /// <summary>
-    /// チャンネル情報を保持するAtomCollectionから新しいチャンネル情報を初期化します
-    /// </summary>
-    /// <param name="chaninfo">チャンネル情報を保持するAtomCollection</param>
-    public ChannelInfo(IAtomCollection chaninfo)
-    {
-      extra = new ReadOnlyAtomCollection(new AtomCollection(chaninfo));
-    }
-  }
-
-  /// <summary>
-  /// チャンネルのトラック情報を保持するクラスです
-  /// </summary>
-  public class ChannelTrack
-  {
-    /// <summary>
-    /// タイトルを取得します
-    /// </summary>
-    public string Name {
-      get {
-        return extra.GetChanTrackTitle();
-      }
-    }
-
-    /// <summary>
-    /// アルバム名を取得します
-    /// </summary>
-    public string Album {
-      get {
-        return extra.GetChanTrackAlbum();
-      }
-    }
-
-    /// <summary>
-    /// ジャンルを取得します
-    /// </summary>
-    public string Genre {
-      get {
-        return extra.GetChanTrackGenre();
-      }
-    }
-
-    /// <summary>
-    /// 作者名を取得します
-    /// </summary>
-    public string Creator {
-      get {
-        return extra.GetChanTrackCreator();
-      }
-    }
-
-    /// <summary>
-    /// トラック情報に関するURLを取得します
-    /// </summary>
-    public string URL {
-      get {
-        return extra.GetChanTrackURL();
-      }
-    }
-
-    private ReadOnlyAtomCollection extra;
-    /// <summary>
-    /// その他のトラック情報を保持するリストを取得します
-    /// </summary>
-    public IAtomCollection Extra { get { return extra; } }
-
-    /// <summary>
-    /// トラック情報を保持するAtomCollectionから新しいトラック情報を初期化します
-    /// </summary>
-    /// <param name="chantrack">トラック情報を保持するAtomCollection</param>
-    public ChannelTrack(IAtomCollection chantrack)
-    {
-      extra = new ReadOnlyAtomCollection(new AtomCollection(chantrack));
-    }
-  }
 
   /// <summary>
   /// Broadcastの送信先を指定します
@@ -248,53 +42,13 @@ namespace PeerCastStation.Core
   }
 
   /// <summary>
-  /// 出力ストリームを保持するコレクションクラスです
-  /// </summary>
-  [Serializable]
-  public class OutputStreamCollection : Collection<IOutputStream>
-  {
-    /// <summary>
-    /// 空のOutputStreamCollectionを初期化します
-    /// </summary>
-    public OutputStreamCollection()
-      : base()
-    {
-    }
-
-    /// <summary>
-    /// 指定したリストから要素をコピーしてOutputStreamCollectionを初期化します
-    /// </summary>
-    /// <param name="list">コピー元のリスト</param>
-    public OutputStreamCollection(IEnumerable<IOutputStream> list)
-      : base(new List<IOutputStream>(list))
-    {
-    }
-  }
-
-  /// <summary>
-  /// 出力ストリームを保持するコレクションの読み取り専用ラッパクラスです
-  /// </summary>
-  [Serializable]
-  public class ReadOnlyOutputStreamCollection : ReadOnlyCollection<IOutputStream>
-  {
-    /// <summary>
-    /// 元になるコレクションを指定してReadOnlyOutputStreamCollectionを初期化します
-    /// </summary>
-    /// <param name="collection">元になるコレクション</param>
-    public ReadOnlyOutputStreamCollection(OutputStreamCollection collection)
-      : base(collection)
-    {
-    }
-  }
-
-  /// <summary>
   /// チャンネル接続を管理するクラスです
   /// </summary>
   public class Channel
   {
     private const int NodeLimit = 180000; //ms
     private ISourceStream sourceStream = null;
-    private OutputStreamCollection outputStreams = new OutputStreamCollection();
+    private List<IOutputStream> outputStreams = new List<IOutputStream>();
     private List<Host> nodes = new List<Host>();
     private Content contentHeader = null;
     private ContentCollection contents = new ContentCollection();
@@ -330,17 +84,11 @@ namespace PeerCastStation.Core
     public Uri SourceUri { get; private set; }
 
     /// <summary>
-    /// ソースストリームを取得および設定します
+    /// ソースストリームを取得します
     /// </summary>
     public ISourceStream SourceStream
     {
       get { return sourceStream; }
-      set
-      {
-        if (sourceStream != value) {
-          sourceStream = value;
-        }
-      }
     }
 
     /// <summary>
@@ -374,8 +122,8 @@ namespace PeerCastStation.Core
     /// <summary>
     /// 出力ストリームの読み取り専用リストを取得します
     /// </summary>
-    public ReadOnlyOutputStreamCollection OutputStreams {
-      get { return new ReadOnlyOutputStreamCollection(outputStreams); }
+    public ReadOnlyCollection<IOutputStream> OutputStreams {
+      get { return new ReadOnlyCollection<IOutputStream>(outputStreams); }
     }
 
     public event EventHandler OutputStreamsChanged;
@@ -386,7 +134,7 @@ namespace PeerCastStation.Core
     public void AddOutputStream(IOutputStream stream)
     {
       Utils.ReplaceCollection(ref outputStreams, orig => {
-        var new_collection = new OutputStreamCollection(outputStreams);
+        var new_collection = new List<IOutputStream>(outputStreams);
         new_collection.Add(stream);
         return new_collection;
       });
@@ -401,7 +149,7 @@ namespace PeerCastStation.Core
     {
       bool removed = false;
       Utils.ReplaceCollection(ref outputStreams, orig => {
-        var new_collection = new OutputStreamCollection(outputStreams);
+        var new_collection = new List<IOutputStream>(outputStreams);
         removed = new_collection.Remove(stream);
         return new_collection;
       });
@@ -640,7 +388,7 @@ namespace PeerCastStation.Core
       foreach (var os in outputStreams) {
         os.Stop();
       }
-      outputStreams = new OutputStreamCollection();
+      outputStreams = new List<IOutputStream>();
       startTickCount = null;
       IsClosed = true;
       OnClosed(args.StopReason);
@@ -704,7 +452,7 @@ namespace PeerCastStation.Core
         foreach (var outputStream in outputStreams) {
           outputStream.Stop();
         }
-        outputStreams = new OutputStreamCollection();
+        outputStreams = new List<IOutputStream>();
       }
     }
 
---
+++ PeerCastStation/PeerCastStation.Core/ChannelInfo.cs
@@ -0,0 +1,184 @@
+﻿using System;
+
+namespace PeerCastStation.Core
+{
+  /// <summary>
+  /// チャンネルのメタデータを保持するクラスです
+  /// </summary>
+  public class ChannelInfo
+  {
+    /// <summary>
+    /// チャンネル名を取得します
+    /// </summary>
+    public string Name {
+      get { return extra.GetChanInfoName(); }
+    }
+
+    /// <summary>
+    /// チャンネルストリームの内容種類を取得します
+    /// </summary>
+    public string ContentType {
+      get { return extra.GetChanInfoType(); }
+    }
+
+    /// <summary>
+    /// ジャンルを取得します
+    /// </summary>
+    public string Genre {
+      get { return extra.GetChanInfoGenre(); }
+    }
+
+    /// <summary>
+    /// チャンネル詳細を取得します
+    /// </summary>
+    public string Desc {
+      get { return extra.GetChanInfoDesc(); }
+    }
+
+    /// <summary>
+    /// 配信コメントを取得します
+    /// </summary>
+    public string Comment {
+      get { return extra.GetChanInfoComment(); }
+    }
+
+    /// <summary>
+    /// コンタクトURLを取得します
+    /// </summary>
+    public string URL {
+      get { return extra.GetChanInfoURL(); }
+    }
+
+    /// <summary>
+    /// 配信ビットレート情報を取得します
+    /// </summary>
+    public int Bitrate {
+      get { return extra.GetChanInfoBitrate() ?? 0; }
+    }
+
+    /// <summary>
+    /// ストリームのMIME Typeを取得します。
+    /// </summary>
+    public string MIMEType {
+      get {
+        var stream_type = extra.GetChanInfoStreamType();
+        if (!String.IsNullOrEmpty(stream_type)) {
+          return stream_type;
+        }
+        else {
+          switch (ContentType) {
+          case "MP3": return "audio/mpeg";
+          case "OGG": return "audio/ogg";
+          case "OGM": return "video/ogg";
+          case "RAW": return "application/octet-stream";
+          case "NSV": return "video/nsv";
+          case "WMA": return "audio/x-ms-wma";
+          case "WMV": return "video/x-ms-wmv";
+          case "PLS": return "audio/mpegurl";
+          case "M3U": return "audio/m3u";
+          case "ASX": return "video/x-ms-asf";
+          default: return "application/octet-stream";
+          }
+        }
+      }
+    }
+
+    /// <summary>
+    /// ストリームファイルの拡張子を取得します
+    /// </summary>
+    public string ContentExtension {
+      get {
+        var stream_ext = extra.GetChanInfoStreamExt();
+        if (!String.IsNullOrEmpty(stream_ext)) {
+          return stream_ext;
+        }
+        else {
+          switch (ContentType) {
+          case "MP3": return ".mp3";
+          case "OGG": return ".ogg";
+          case "OGM": return ".ogv";
+          case "RAW": return "";
+          case "NSV": return ".nsv";
+          case "WMA": return ".wma";
+          case "WMV": return ".wmv";
+          case "PLS": return ".pls";
+          case "M3U": return ".m3u";
+          case "ASX": return ".asx";
+          default: return "";
+          }
+        }
+      }
+    }
+
+    private ReadOnlyAtomCollection extra;
+    /// <summary>
+    /// その他のチャンネル情報を保持するリストを取得します
+    /// </summary>
+    public IAtomCollection Extra { get { return extra; } }
+
+    /// <summary>
+    /// チャンネル情報を保持するAtomCollectionから新しいチャンネル情報を初期化します
+    /// </summary>
+    /// <param name="chaninfo">チャンネル情報を保持するAtomCollection</param>
+    public ChannelInfo(IAtomCollection chaninfo)
+    {
+      extra = new ReadOnlyAtomCollection(new AtomCollection(chaninfo));
+    }
+  }
+
+  /// <summary>
+  /// チャンネルのトラック情報を保持するクラスです
+  /// </summary>
+  public class ChannelTrack
+  {
+    /// <summary>
+    /// タイトルを取得します
+    /// </summary>
+    public string Name {
+      get { return extra.GetChanTrackTitle(); }
+    }
+
+    /// <summary>
+    /// アルバム名を取得します
+    /// </summary>
+    public string Album {
+      get { return extra.GetChanTrackAlbum(); }
+    }
+
+    /// <summary>
+    /// ジャンルを取得します
+    /// </summary>
+    public string Genre {
+      get { return extra.GetChanTrackGenre(); }
+    }
+
+    /// <summary>
+    /// 作者名を取得します
+    /// </summary>
+    public string Creator {
+      get { return extra.GetChanTrackCreator(); }
+    }
+
+    /// <summary>
+    /// トラック情報に関するURLを取得します
+    /// </summary>
+    public string URL {
+      get { return extra.GetChanTrackURL(); }
+    }
+
+    private ReadOnlyAtomCollection extra;
+    /// <summary>
+    /// その他のトラック情報を保持するリストを取得します
+    /// </summary>
+    public IAtomCollection Extra { get { return extra; } }
+
+    /// <summary>
+    /// トラック情報を保持するAtomCollectionから新しいトラック情報を初期化します
+    /// </summary>
+    /// <param name="chantrack">トラック情報を保持するAtomCollection</param>
+    public ChannelTrack(IAtomCollection chantrack)
+    {
+      extra = new ReadOnlyAtomCollection(new AtomCollection(chantrack));
+    }
+  }
+}
