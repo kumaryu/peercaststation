26a7b9db87fd7568229372aee7c142f53195f0df
kumaryu <kumaryu@kumaryu.net>
2013-09-20 17:25:05 +0900

    GUIでもポート毎の認証有無とID・パスワードを編集できるようにした

--- PeerCastStation/PeerCastStation.WPF/CoreSettings/Dialogs/ListenerEditViewModel.cs
+++ PeerCastStation/PeerCastStation.WPF/CoreSettings/Dialogs/ListenerEditViewModel.cs
@@ -14,27 +14,79 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net;
 using System.Net.Sockets;
-using System.Text;
 using PeerCastStation.Core;
 using PeerCastStation.WPF.Commons;
+using System.ComponentModel;
 
 namespace PeerCastStation.WPF.CoreSettings.Dialogs
 {
   class ListenerEditViewModel
+    : INotifyPropertyChanged
   {
-    public string Address { get; set; }
-    public int Port { get; set; }
+    public string address;
+    public string Address {
+      get { return address; }
+      set { if (address!=value) { address = value; OnPropertyChanged("Address"); } }
+    }
+    public int port;
+    public int Port {
+      get { return port; }
+      set { if (port!=value) { port = value; OnPropertyChanged("Port"); } }
+    }
 
-    public bool IsLocalRelay { get; set; }
-    public bool IsLocalDirect { get; set; }
-    public bool IsLocalInterface { get; set; }
-    public bool IsGlobalRelay { get; set; }
-    public bool IsGlobalDirect { get; set; }
-    public bool IsGlobalInterface { get; set; }
+    public bool localRelay;
+    public bool LocalRelay {
+      get { return localRelay; }
+      set { if (localRelay!=value) { localRelay = value; OnPropertyChanged("LocalRelay"); } }
+    }
+    public bool localPlay;
+    public bool LocalPlay {
+      get { return localPlay; }
+      set { if (localPlay!=value) { localPlay = value; OnPropertyChanged("LocalPlay"); } }
+    }
+    public bool localInterface;
+    public bool LocalInterface {
+      get { return localInterface; }
+      set { if (localInterface!=value) { localInterface = value; OnPropertyChanged("LocalInterface"); } }
+    }
+    public bool localAuthRequired;
+    public bool LocalAuthRequired {
+      get { return localAuthRequired; }
+      set { if (localAuthRequired!=value) { localAuthRequired = value; OnPropertyChanged("LocalAuthRequired"); } }
+    }
+    public bool globalRelay;
+    public bool GlobalRelay {
+      get { return globalRelay; }
+      set { if (globalRelay!=value) { globalRelay = value; OnPropertyChanged("GlobalRelay"); } }
+    }
+    public bool globalPlay;
+    public bool GlobalPlay {
+      get { return globalPlay; }
+      set { if (globalPlay!=value) { globalPlay = value; OnPropertyChanged("GlobalPlay"); } }
+    }
+    public bool globalInterface;
+    public bool GlobalInterface {
+      get { return globalInterface; }
+      set { if (globalInterface!=value) { globalInterface = value; OnPropertyChanged("GlobalInterface"); } }
+    }
+    public bool globalAuthRequired;
+    public bool GlobalAuthRequired {
+      get { return globalAuthRequired; }
+      set { if (globalAuthRequired!=value) { globalAuthRequired = value; OnPropertyChanged("GlobalAuthRequired"); } }
+    }
+    public string authId;
+    public string AuthId {
+      get { return authId; }
+      set { if (authId!=value) { authId = value; OnPropertyChanged("AuthId"); } }
+    }
+    public string authPassword;
+    public string AuthPassword {
+      get { return authPassword; }
+      set { if (authPassword!=value) { authPassword = value; OnPropertyChanged("AuthPassword"); } }
+    }
+    public System.Windows.Input.ICommand RegenerateAuthKey { get; private set; }
 
     private readonly Command add;
     public Command Add { get { return add; } }
@@ -43,10 +95,19 @@ namespace PeerCastStation.WPF.CoreSettings.Dialogs
     {
       Address = "IPv4 Any";
       Port = 7144;
-      IsLocalRelay = true;
-      IsLocalDirect = true;
-      IsLocalInterface = true;
-      IsGlobalRelay = true;
+      LocalRelay = true;
+      LocalPlay = true;
+      LocalInterface = true;
+      GlobalRelay = true;
+      GlobalAuthRequired = true;
+      var key = AuthenticationKey.Generate();
+      AuthId       = key.Id;
+      AuthPassword = key.Password;
+      RegenerateAuthKey = new Command(() => {
+        key = AuthenticationKey.Generate();
+        AuthId       = key.Id;
+        AuthPassword = key.Password;
+      });
 
       add = new Command(() =>
       {
@@ -60,13 +121,16 @@ namespace PeerCastStation.WPF.CoreSettings.Dialogs
           return;
         }
         var localAccepts = ToOutputStreamType(
-          IsLocalRelay, IsLocalDirect, IsLocalInterface);
+          LocalRelay, LocalPlay, LocalInterface);
         var glocalAccepts = ToOutputStreamType(
-          IsGlobalRelay, IsGlobalDirect, IsGlobalInterface);
+          GlobalRelay, GlobalPlay, GlobalInterface);
         try
         {
-          peerCast.StartListen(
+          var listener = peerCast.StartListen(
             new IPEndPoint(address, Port), localAccepts, glocalAccepts);
+          listener.LocalAuthorizationRequired = LocalAuthRequired;
+          listener.GlobalAuthorizationRequired = GlobalAuthRequired;
+          listener.AuthenticationKey = new AuthenticationKey(AuthId, AuthPassword);
         }
         catch (SocketException)
         {
@@ -95,5 +159,14 @@ namespace PeerCastStation.WPF.CoreSettings.Dialogs
       if (interface_) type |= OutputStreamType.Interface;
       return type;
     }
+
+    private void OnPropertyChanged(string name)
+    {
+      if (PropertyChanged!=null) {
+        PropertyChanged(this, new PropertyChangedEventArgs(name));
+      }
+    }
+
+    public event PropertyChangedEventHandler  PropertyChanged;
   }
 }
