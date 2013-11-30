c64dd22c6f8b9612718698d16eedd2d209796efe
kumaryu <kumaryu@kumaryu.net>
2013-11-30 21:02:56 +0900

    Bumped version up tp 1.6.2.0

--- PeerCastStation/PeerCastStation/Properties/Settings.Designer.cs
+++ PeerCastStation/PeerCastStation/Properties/Settings.Designer.cs
@@ -67,7 +67,7 @@ namespace PeerCastStation.Properties {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("PeerCastStation/1.6.1.0")]
+        [global::System.Configuration.DefaultSettingValueAttribute("PeerCastStation/1.6.2.0")]
         public string AgentName {
             get {
                 return ((string)(this["AgentName"]));
