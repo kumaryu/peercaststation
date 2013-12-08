799a6c0c815535577d147c79febbe5a6071cadd7
kumaryu <kumaryu@kumaryu.net>
2013-12-08 21:40:16 +0900

    app.configのappSettingsセクションを読み込むAppSettingsReaderを追加した
    アプリケーションの構成ファイルから直接読み込みができるようにappSettingsセクションから値を取得するAppSettingsReaderクラスを追加した。

---
+++ PeerCastStation/PeerCastStation.Core/AppSettingsReader.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Configuration;
+
+namespace PeerCastStation.Core
+{
+  public class AppSettingsReader
+  {
+    static public bool TryGetDate(string key, out DateTime value)
+    {
+      try {
+        var v = System.Configuration.ConfigurationManager.AppSettings[key];
+        return DateTime.TryParse(v, out value);
+      }
+      catch (System.Configuration.ConfigurationException) {
+        value = DateTime.Today;
+        return false;
+      }
+    }
+
+    static public DateTime GetDate(string key, DateTime default_value)
+    {
+      DateTime res;
+      if (TryGetDate(key, out res)) return res;
+      else                          return default_value;
+    }
+
+    static public bool TryGetString(string key, out string value)
+    {
+      try {
+        value = System.Configuration.ConfigurationManager.AppSettings[key];
+        return value!=null;
+      }
+      catch (System.Configuration.ConfigurationException) {
+        value = null;
+        return false;
+      }
+    }
+
+    static public string GetString(string key, string default_value)
+    {
+      string res;
+      if (TryGetString(key, out res)) return res;
+      else                            return default_value;
+    }
+
+    static public bool TryGetUri(string key, out Uri value)
+    {
+      try {
+        var v = System.Configuration.ConfigurationManager.AppSettings[key];
+        return Uri.TryCreate(v, UriKind.Absolute, out value);
+      }
+      catch (System.Configuration.ConfigurationException) {
+        value = null;
+        return false;
+      }
+    }
+
+    static public Uri GetUri(string key, Uri default_value)
+    {
+      Uri res;
+      if (TryGetUri(key, out res)) return res;
+      else                         return default_value;
+    }
+  }
+
+}
