b26ec61b1fdd84853c4ddff2a1bc5a1f141796d3
kumaryu <kumaryu@kumaryu.net>
2014-03-16 22:11:58 +0900

    ファイルの実行ブロックを一括解除するunblock.batを追加した
    ダウンロードしてzipを解凍すると、zip内のdllやexeにZoneIdがついて実行や読み込みがブロックされるので、
    必要なdllやexeのブロックを一括解除できるunblock.batを追加した。

---
+++ PeerCastStation/PeerCastStation/unblock.bat
@@ -0,0 +1,16 @@
+@cd %~dp0
+echo. > PeerCastStation.exe:Zone.Identifier
+echo. > Newtonsoft.Json.dll:Zone.Identifier
+echo. > PeerCastStation.ASF.dll:Zone.Identifier
+echo. > PeerCastStation.Core.dll:Zone.Identifier
+echo. > PeerCastStation.FLV.dll:Zone.Identifier
+echo. > PeerCastStation.GUI.dll:Zone.Identifier
+echo. > PeerCastStation.HTTP.dll:Zone.Identifier
+echo. > PeerCastStation.MKV.dll:Zone.Identifier
+echo. > PeerCastStation.PCP.dll:Zone.Identifier
+echo. > PeerCastStation.UI.HTTP.dll:Zone.Identifier
+echo. > PeerCastStation.WPF.dll:Zone.Identifier
+echo. > WPFToolkit.dll:Zone.Identifier
+echo. > Xceed.Wpf.Toolkit.dll:Zone.Identifier
+pause
+
