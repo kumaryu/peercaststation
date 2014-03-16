cb5aef967f51d8e69fe14c50efd76dfd93710f2e
kumaryu <kumaryu@kumaryu.net>
2014-03-16 23:29:56 +0900

    Bumped version up to 1.7.9.1

--- PeerCastStation/PeerCastStation.GUI/Properties/AssemblyInfo.cs
+++ PeerCastStation/PeerCastStation.GUI/Properties/AssemblyInfo.cs
@@ -48,5 +48,5 @@ using System.Runtime.InteropServices;
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.7.9.0")]
-[assembly: AssemblyInformationalVersion("1.7.9.0")]
+[assembly: AssemblyFileVersion("1.7.9.1")]
+[assembly: AssemblyInformationalVersion("1.7.9.1")]
