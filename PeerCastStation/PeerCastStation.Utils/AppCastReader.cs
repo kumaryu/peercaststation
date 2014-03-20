b9346d8ba4d2a1745b1c71c99ae831b353c11373
kumaryu <kumaryu@kumaryu.net>
2014-03-20 12:17:13 +0900

    GUIから更新確認を切り離すためにPeerCastStation.Utilsを追加しUpdaterクラスを入れた
    更新確認はWPF版GUIでのみ動くようになっていたがHTML UIでも更新確認を行えるようにするため、更新確認を切り出してPeerCastStation.Utils.Updaterを追加した。

---
+++ PeerCastStation/PeerCastStation.Utils/AppCastReader.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+using System.Net;
+using PeerCastStation.Core;
+
+namespace PeerCastStation.Utils
+{
+  internal class AppCastReader
+  {
+    private Uri url;
+    private WebClient client;
+    private Action<IEnumerable<VersionDescription>> downloaded;
+    public AppCastReader()
+    {
+      this.client = new WebClient();
+      this.client.DownloadDataCompleted += OnDownloadDataCompleted;
+    }
+
+    public bool DownloadVersionInfoAsync(Uri source, Action<IEnumerable<VersionDescription>> handler)
+    {
+      if (this.client.IsBusy) return false;
+      this.downloaded = handler;
+      this.client.DownloadDataAsync(url);
+      return true;
+    }
+
+    private void OnDownloadDataCompleted(object sender, DownloadDataCompletedEventArgs args)
+    {
+      if (args.Cancelled || args.Error!=null) return;
+      var data = System.Text.Encoding.UTF8.GetString(args.Result);
+      var doc = XDocument.Parse(data);
+      var versions = new List<VersionDescription>();
+      foreach (var item in doc.Descendants("item")) {
+        var xtitle = item.Element("title");
+        var xdate  = item.Element("pubDate");
+        var xlink  = item.Element("link");
+        var xdesc  = item.Element("description");
+        DateTime date;
+        Uri    link  = null;
+        string title = null;
+        string desc  = null;
+        if (xtitle!=null && xtitle.Value!=null) {
+          title = xtitle.Value;
+        }
+        if (xlink!=null && xlink.Value!=null) {
+          Uri.TryCreate(xlink.Value, UriKind.Absolute, out link);
+        }
+        if (xdesc!=null && xdesc.Value!=null) {
+          desc = xdesc.ToString();
+        }
+        if (xdate!=null && xdate.Value!=null && DateTime.TryParse(xdate.Value, out date)) {
+          versions.Add(new VersionDescription {
+            Title       = title,
+            PublishDate = date,
+            Link        = link,
+            Description = desc,
+          });
+        }
+        if (downloaded!=null) {
+          downloaded(versions);
+        }
+      }
+    }
+
+  }
+}
---
+++ PeerCastStation/PeerCastStation.Utils/Updater.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using PeerCastStation.Core;
+
+namespace PeerCastStation.Utils
+{
+  public class VersionDescription
+  {
+    public DateTime PublishDate { get; set; }
+    public Uri      Link        { get; set; }
+    public string   Title       { get; set; }
+    public string   Description { get; set; }
+  }
+
+  public class NewVersionFoundEventArgs : EventArgs
+  {
+    public IEnumerable<VersionDescription> VersionDescriptions { get; private set; }
+    public NewVersionFoundEventArgs(IEnumerable<VersionDescription> desc)
+    {
+      this.VersionDescriptions = desc;
+    }
+  }
+  public delegate void NewVersionFoundEventHandler(object sender, NewVersionFoundEventArgs args);
+
+  public class Updater
+  {
+    private Uri url;
+    private DateTime currentVersion;
+    private PeerCastStation.Utils.AppCastReader appcastReader = new AppCastReader();
+    public Updater()
+    {
+      this.url            = AppSettingsReader.GetUri("UpdateUrl", new Uri("http://www.pecastation.org/files/appcast.xml"));
+      this.currentVersion = AppSettingsReader.GetDate("CurrentVersion", DateTime.Today);
+    }
+
+    public bool CheckVersion()
+    {
+      return appcastReader.DownloadVersionInfoAsync(url, desc => {
+        var new_versions = desc
+          .Where(v => v.PublishDate<currentVersion)
+          .OrderByDescending(v => v.PublishDate);
+        if (new_versions.Count()>0 && NewVersionFound!=null) {
+          NewVersionFound(this, new NewVersionFoundEventArgs(new_versions));
+        }
+      });
+    }
+
+    public event NewVersionFoundEventHandler NewVersionFound;
+  }
+}
