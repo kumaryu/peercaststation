e2218ae4197fc7cb889741ca4b099d85c841de20
kumaryu <kumaryu@kumaryu.net>
2014-03-25 21:05:19 +0900

    Utils.csにRateCounterが残っていたのを削除した

--- PeerCastStation/PeerCastStation.Core/Utils.cs
+++ PeerCastStation/PeerCastStation.Core/Utils.cs
@@ -124,43 +124,4 @@ namespace PeerCastStation.Core
     }
   }
 
-  public class RateCounter
-  {
-    private long count;
-    private int begin;
-    private float rate;
-    public int Duration { get; private set; }
-    public float Rate { get { Check(); return rate; } }
-
-    public RateCounter(int duration)
-    {
-      this.Duration = duration;
-      this.begin = Environment.TickCount-duration;
-    }
-
-    public void Add(int value)
-    {
-      count += value;
-      Check();
-    }
-
-    public void Reset()
-    {
-      rate = 0;
-      count = 0;
-      begin = Environment.TickCount;
-    }
-
-    private void Check()
-    {
-      var t = Environment.TickCount;
-      var d = Math.Abs(t-begin);
-      if (d>Duration) {
-        rate = count*1000 / (float)d;
-        count = 0;
-        begin = Environment.TickCount;
-      }
-    }
-  }
-
 }
