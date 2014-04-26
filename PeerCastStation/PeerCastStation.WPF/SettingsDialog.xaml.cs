ac89dfc268aa0fb5670c25f9f77118f929a412a8
kumaryu <kumaryu@kumaryu.net>
2014-04-26 22:53:30 +0900

    メインウィンドウにメニューを追加し、設定とログのタブを分離した
    メインウィンドウにあまり使わないにもかかわらず設定とログがタブで入っており、逆にメニューがなかったので、
    メニューを追加して設定とログは別ウィンドウに分離した。

---
+++ PeerCastStation/PeerCastStation.WPF/SettingsDialog.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+
+namespace PeerCastStation.WPF
+{
+  /// <summary>
+  /// SettingsDialog.xaml の相互作用ロジック
+  /// </summary>
+  public partial class SettingsDialog : Window
+  {
+    public SettingsDialog()
+    {
+      InitializeComponent();
+    }
+
+    internal static void ShowDialog(Window owner, MainViewModel vm)
+    {
+      var window = new SettingsDialog { DataContext=vm.Setting };
+      window.Owner = owner;
+      window.ShowDialog();
+    }
+
+    private void Button_Click(object sender, RoutedEventArgs e)
+    {
+      this.Close();
+    }
+  }
+}
