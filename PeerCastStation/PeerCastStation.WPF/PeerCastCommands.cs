131f03894821b7fd4bf16ef2d23250e287e9f65c
kumaryu <kumaryu@kumaryu.net>
2014-04-26 22:53:30 +0900

    メインウィンドウにメニューを追加し、設定とログのタブを分離した
    メインウィンドウにあまり使わないにもかかわらず設定とログがタブで入っており、逆にメニューがなかったので、
    メニューを追加して設定とログは別ウィンドウに分離した。

---
+++ PeerCastStation/PeerCastStation.WPF/PeerCastCommands.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace PeerCastStation.WPF
+{
+  internal static class PeerCastCommands
+  {
+    private static RoutedUICommand startBroadcasting;
+    private static RoutedUICommand openSettings;
+    private static RoutedUICommand showLogs;
+    public static RoutedUICommand StartBroadcasting { get { return startBroadcasting; } }
+    public static RoutedUICommand OpenSettings      { get { return openSettings; } }
+    public static RoutedUICommand ShowLogs          { get { return showLogs; } }
+
+    static PeerCastCommands()
+    {
+      startBroadcasting = new RoutedUICommand("配信(_B)...", "StartBroadcasting", typeof(PeerCastCommands));
+      openSettings      = new RoutedUICommand("設定(_S)...", "OpenSettings", typeof(PeerCastCommands));
+      showLogs          = new RoutedUICommand("ログの表示(_L)...", "ShowLogs", typeof(PeerCastCommands));
+    }
+  }
+}
