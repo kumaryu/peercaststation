351a44a26a608f595e245a32ee41fa44337c7075
kumaryu <kumaryu@kumaryu.net>
2014-05-06 00:15:01 +0900

    設定ダイアログの構成を大幅に変更した
    設定ダイアログにOK・キャンセル・適用ボタンを追加して、明示的にOKか適用するまで実際に値が反映されないようにした。
    この変更に伴い、接続待ち受けポートと掲載用YPの一覧編集が緩和されたため、別ダイアログでなくインラインで編集できるようにした。

--- PeerCastStation/PeerCastStation.WPF/CoreSettings/SettingControl.xaml.cs
+++ PeerCastStation/PeerCastStation.WPF/CoreSettings/SettingControl.xaml.cs
@@ -38,33 +38,6 @@ namespace PeerCastStation.WPF.CoreSettings
     public SettingControl()
     {
       InitializeComponent();
-
-      Ports.AddItemButton.Click += (sender, e) =>
-        {
-          var dialog = new ListenerEditWindow
-          {
-            Owner = Window.GetWindow(this),
-            DataContext = ((SettingViewModel)DataContext).ListenerEdit
-          };
-          dialog.ShowDialog();
-          Ports.GetBindingExpression(UserControl.DataContextProperty).UpdateTarget();
-        };
-
-      YellowPagesList.AddItemButton.Click += (sender, e) =>
-        {
-          var dialog = new YellowPagesEditWindow
-          {
-            Owner = Window.GetWindow(this),
-            DataContext = ((SettingViewModel)DataContext).YellowPagesEdit
-          };
-          dialog.ShowDialog();
-          YellowPagesList.GetBindingExpression(UserControl.DataContextProperty).UpdateTarget();
-        };
-    }
-
-    private void AddYellowPagesButton_Click(object sender, RoutedEventArgs e)
-    {
-      MessageBox.Show("実装ないのねーん");
     }
   }
 }
