12fd99227da9406043827518b066613a44c0fffa
kumaryu <kumaryu@kumaryu.net>
2014-05-09 21:43:19 +0900

    上り帯域測定状況を別ダイアログで表示するようにした
    上り帯域測定状況は測定開始ボタンの隣に表示していたが、レイアウト的に厳しくなったのと、結構時間がかかるわりに変化がなかったので、別ダイアログで状況と結果の表示をするようにした。

---
+++ PeerCastStation/PeerCastStation.WPF/CoreSettings/BandwidthCheckDialog.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using PeerCastStation.Core;
+
+namespace PeerCastStation.WPF.CoreSettings
+{
+  /// <summary>
+  /// BandwidthCheckDialog.xaml の相互作用ロジック
+  /// </summary>
+  public partial class BandwidthCheckDialog : Window
+  {
+    private BandwidthChecker checker;
+    public BandwidthCheckDialog()
+    {
+      InitializeComponent();
+      this.DataContext = this;
+      Uri target_uri;
+      if (AppSettingsReader.TryGetUri("BandwidthChecker", out target_uri)) {
+        this.checker = new BandwidthChecker(target_uri);
+        this.checker.BandwidthCheckCompleted += checker_BandwidthCheckCompleted;
+        this.checker.RunAsync();
+        this.Status = "帯域測定中";
+      }
+      else {
+        this.IsChecking = false;
+        this.Status = "接続先設定が取得できません";
+      }
+    }
+
+    public string Status {
+      get { return (string)GetValue(StatusProperty); }
+      set { SetValue(StatusProperty, value); }
+    }
+    public static readonly DependencyProperty StatusProperty = 
+      DependencyProperty.Register("Status", typeof(string), typeof(BandwidthCheckDialog), new PropertyMetadata(""));
+
+    public bool IsChecking {
+      get { return (bool)GetValue(IsCheckingProperty); }
+      set { SetValue(IsCheckingProperty, value); }
+    }
+    public static readonly DependencyProperty IsCheckingProperty = 
+      DependencyProperty.Register("IsChecking", typeof(bool), typeof(BandwidthCheckDialog), new PropertyMetadata(true));
+
+    public int? Result { get; private set; }
+
+    private void checker_BandwidthCheckCompleted(
+        object sender,
+        BandwidthCheckCompletedEventArgs args)
+    {
+      if (args.Success) {
+        Result = (int)((args.Bitrate / 1000) * 0.8 / 100) * 100;
+        Status = String.Format("帯域測定完了: {0}kbps, 設定推奨値: {1}kbps",
+          args.Bitrate/1000,
+          (int)((args.Bitrate / 1000) * 0.8 / 100) * 100);
+      }
+      else {
+        Status = "帯域測定失敗。接続できませんでした";
+      }
+      IsChecking = false;
+    }
+
+    private void Button_Click(object sender, RoutedEventArgs e)
+    {
+      this.Close();
+    }
+
+  }
+}
