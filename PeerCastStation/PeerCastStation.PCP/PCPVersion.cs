2010291f97ca162ab26ae4e69d01de13b9cdee12
kumaryu <kumaryu@kumaryu.net>
2015-05-09 16:57:46 +0900

    Bumped version up to 1.8.1

--- PeerCastStation/PeerCastStation.PCP/PCPVersion.cs
+++ PeerCastStation/PeerCastStation.PCP/PCPVersion.cs
@@ -9,7 +9,7 @@ namespace PeerCastStation.PCP
     public static readonly int    ServantVersion         = 1218;
     public static readonly int    ServantVersionVP       = 27;
     public static readonly byte[] ServantVersionEXPrefix = new byte[] { (byte)'S', (byte)'T' };
-    public static readonly short  ServantVersionEXNumber = 180;
+    public static readonly short  ServantVersionEXNumber = 181;
 
     public static void SetHeloVersion(IAtomCollection helo)
     {
