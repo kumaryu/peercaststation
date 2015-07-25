8dcc29be9f8965f4d386d996ceb77ec4d263811b
kumaryu <kumaryu@kumaryu.net>
2015-07-25 13:29:19 +0900

    サービスのデバッグは大変なので、擬似的にサービスのデバッグをできるPecaStationdDebugを追加した

---
+++ PeerCastStation/PecaStationdDebug/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace PecaStationdDebug
+{
+  class Program
+	{
+		static void Main(string[] args)
+		{
+			var app = new PecaStationd.PeerCastStationService.PecaServiceApplication();
+			app.Start();
+			System.Console.ReadLine();
+			app.Stop();
+		}
+	}
+}
