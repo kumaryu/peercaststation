9e31c8066d98f9e65a538b74160e2346897e046a
kumaryu <kumaryu@kumaryu.net>
2015-12-26 21:20:04 +0900

    PecaStationdのインストーラを追加した

--- PeerCastStation/PecaStationd/ProjectInstaller.Designer.cs
+++ PeerCastStation/PecaStationd/ProjectInstaller.Designer.cs
@@ -27,25 +27,28 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.peerCastStationServiceProcessInstaller = new System.ServiceProcess.ServiceProcessInstaller();
-			this.peerCastStationServiceInstaller = new System.ServiceProcess.ServiceInstaller();
-			// 
-			// peerCastStationServiceProcessInstaller
-			// 
-			this.peerCastStationServiceProcessInstaller.Account = System.ServiceProcess.ServiceAccount.LocalService;
-			this.peerCastStationServiceProcessInstaller.Password = null;
-			this.peerCastStationServiceProcessInstaller.Username = null;
-			// 
-			// peerCastStationServiceInstaller
-			// 
-			this.peerCastStationServiceInstaller.ServiceName = "PeerCastStationService";
-			// 
-			// ProjectInstaller
-			// 
-			this.Installers.AddRange(new System.Configuration.Install.Installer[] {
+      this.peerCastStationServiceProcessInstaller = new System.ServiceProcess.ServiceProcessInstaller();
+      this.peerCastStationServiceInstaller = new System.ServiceProcess.ServiceInstaller();
+      // 
+      // peerCastStationServiceProcessInstaller
+      // 
+      this.peerCastStationServiceProcessInstaller.Account = System.ServiceProcess.ServiceAccount.LocalService;
+      this.peerCastStationServiceProcessInstaller.Password = null;
+      this.peerCastStationServiceProcessInstaller.Username = null;
+      // 
+      // peerCastStationServiceInstaller
+      // 
+      this.peerCastStationServiceInstaller.Description = "PeerCastStation";
+      this.peerCastStationServiceInstaller.DisplayName = "PeerCastStation Service";
+      this.peerCastStationServiceInstaller.ServiceName = "PeerCastStationService";
+      this.peerCastStationServiceInstaller.StartType = System.ServiceProcess.ServiceStartMode.Automatic;
+      // 
+      // ProjectInstaller
+      // 
+      this.Installers.AddRange(new System.Configuration.Install.Installer[] {
             this.peerCastStationServiceProcessInstaller,
             this.peerCastStationServiceInstaller});
-			this.AfterInstall += new System.Configuration.Install.InstallEventHandler(this.ProjectInstaller_AfterInstall);
+      this.AfterInstall += new System.Configuration.Install.InstallEventHandler(this.ProjectInstaller_AfterInstall);
 
 		}
 
