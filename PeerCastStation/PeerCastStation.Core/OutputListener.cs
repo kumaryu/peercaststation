60ee8cae9e89007a15b99cc23308a1a2dc7cf5be
kumaryu <kumaryu@kumaryu.net>
2016-09-25 21:43:01 +0900

    Windows以外では待ち受けソケットにReuseAddressを付けないようにした
    mono 4.6とLinuxでTCPソケットにReuseAddressをつけようとするとSocketExceptionで失敗するようになったので、回避のためにWindows以外ではReuseAddressを付けないようにした。
    WindowsのSO_REUSEADDRは他プラットフォームでのSO_REUSEADDR+SO_REUSEPORT相当になっており、monoでもそうなるようにエミュレーションしているが、Linuxの3.9以前ではSO_REUSEPORTが無いためにmonoではエラーにしているぽい。
    ただmonoでは標準でSO_REUSEADDRをつけていて明示的に指定する必要もないので、WindowsだけでReuseAddressをつけるようにした。

--- PeerCastStation/PeerCastStation.Core/OutputListener.cs
+++ PeerCastStation/PeerCastStation.Core/OutputListener.cs
@@ -161,7 +161,14 @@ namespace PeerCastStation.Core
       UpdateGlobalAccessControlInfo();
       this.ConnectionHandler = connection_handler;
       server = new TcpListener(ip);
-      server.Server.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+      if (Environment.OSVersion.Platform==PlatformID.Win32NT) {
+        //Windowsの時だけReuseAddressをつける。
+        //Windows以外ではReuseAddressがSO_REUSEADDR+SO_REUSEPORT扱いになり
+        //monoの4.6ではLinuxでUDP以外にSO_REUSEPORTを付けようとすると失敗する。
+        //そのかわりmonoではSO_REUSEADDRが標準で付いてるようなので
+        //Windows以外は明示的には付けないようにした。
+        server.Server.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+      }
       server.Start(Int32.MaxValue);
       listenTask = StartListen(server, cancellationSource.Token);
     }
