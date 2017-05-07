dd42ff8585d92e3f156a8e31e8bc84bf91af3127
kumaryu <kumaryu@kumaryu.net>
2017-05-07 22:25:53 +0900

    HTML UIの設定で認証情報を隠せるようにした
    設定画面で認証のIDとパスワードがまる見えで表示されるのはよろしくないので、一応隠せるようにした。

--- PeerCastStation/PeerCastStation.UI.HTTP/html/js/settings.js
+++ PeerCastStation/PeerCastStation.UI.HTTP/html/js/settings.js
@@ -145,6 +145,7 @@ var ListenerViewModel = function(value) {
   self.authenticationPassword = ko.observable(value.authenticationPassword);
   self.authToken              = ko.observable(value.authToken);
   self.isOpened               = ko.observable(value.isOpened);
+  self.authenticationInfoVisibility = ko.observable(false);
   self.authUrl = ko.computed(function () {
     var addr = self.address();
     if (addr=='0.0.0.0' || addr=='0::0') {
@@ -172,6 +173,10 @@ var ListenerViewModel = function(value) {
   self.globalAuthRequired.subscribe(function (value) {
     self.setAuthorizationRequired();
   });
+
+  self.showAuthenticationInfo = function() {
+    self.authenticationInfoVisibility(true);
+  };
 
   self.update = function(data) {
     self.id(data.listenerId);
