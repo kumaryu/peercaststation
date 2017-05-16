39021e17220cef870c48d15c666be6cdc687d17d
kumaryu <kumaryu@kumaryu.net>
2017-05-17 02:18:29 +0900

    配信開始時のビットレートに負数や空文字を設定するとビットレートが正しく設定されないのを修正した
    HTML UIやJSON RPCでの配信開始時にビットレートにデフォルトの空文字を設定すると例外で配信が開始されなかったのを修正した。
    また、PecaStarterではJSON RPCでビットレートに自動設定扱いで-1が指定されるので、0以下の値が来た時はビットレートを指定しないようにした。

--- PeerCastStation/PeerCastStation.UI.HTTP/JObjectExtension.cs
+++ PeerCastStation/PeerCastStation.UI.HTTP/JObjectExtension.cs
@@ -7,33 +7,56 @@ namespace PeerCastStation.UI.HTTP
 {
 	static internal class JObjectExtension
 	{
-		static public void TryGetThen(this JObject self, string property, Action<int> action)
-		{
-			var v = self[property];
-			if (v==null) return;
-			switch (v.Type) {
-			case JTokenType.Null:
-			case JTokenType.Undefined:
-				break;
-			default:
-				action((int)v);
-				break;
-			}
-		}
+    static public void TryGetThen(this JObject self, string property, Action<int> action)
+    {
+      var v = self[property];
+      if (v==null) return;
+      switch (v.Type) {
+      case JTokenType.Null:
+      case JTokenType.Undefined:
+        break;
+      case JTokenType.String:
+        if (String.IsNullOrEmpty((string)v)) {
+          return;
+        }
+        else {
+          int result;
+          if (Int32.TryParse((string)v, out result)) {
+            action(result);
+          }
+          break;
+        }
+      default:
+        action((int)v);
+        break;
+      }
+    }
 
-		static public void TryGetThen(this JObject self, string property, Action<bool> action)
-		{
-			var v = self[property];
-			if (v==null) return;
-			switch (v.Type) {
-			case JTokenType.Null:
-			case JTokenType.Undefined:
-				break;
-			default:
-				action((bool)v);
-				break;
-			}
-		}
+    static public void TryGetThen(this JObject self, string property, Action<bool> action)
+    {
+      var v = self[property];
+      if (v==null) return;
+      switch (v.Type) {
+      case JTokenType.Null:
+      case JTokenType.Undefined:
+        break;
+      case JTokenType.String:
+        if (String.IsNullOrEmpty((string)v)) {
+          return;
+        }
+        else {
+          bool result;
+          if (Boolean.TryParse((string)v, out result)) {
+            action(result);
+          }
+          break;
+        }
+      case JTokenType.Boolean:
+      default:
+        action((bool)v);
+        break;
+      }
+    }
 
 		static public void TryGetThen(this JObject self, string property, Action<string> action)
 		{
