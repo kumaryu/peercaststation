76f25c646b3c9d64bbeb1ef0ecce386a7e635ea1
kumaryu <kumaryu@kumaryu.net>
2017-05-29 21:59:14 +0900

    unblock.batで一通りのdllとexeのブロック解除をするようにした
    unblock.batへのdllとexeの追加を忘れるので、一括でブロック解除をするようにした。
    余計なdllやexeが同じディレクトリにあると勝手にブロック解除されてしまうが、手動で実行するものなので大丈夫なこととする。

--- PeerCastStation/PeerCastStation/unblock.bat
+++ PeerCastStation/PeerCastStation/unblock.bat
@@ -1,16 +1,6 @@
 @cd %~dp0
-echo. > PeerCastStation.exe:Zone.Identifier
-echo. > Newtonsoft.Json.dll:Zone.Identifier
-echo. > PeerCastStation.ASF.dll:Zone.Identifier
-echo. > PeerCastStation.Core.dll:Zone.Identifier
-echo. > PeerCastStation.FLV.dll:Zone.Identifier
-echo. > PeerCastStation.GUI.dll:Zone.Identifier
-echo. > PeerCastStation.HTTP.dll:Zone.Identifier
-echo. > PeerCastStation.MKV.dll:Zone.Identifier
-echo. > PeerCastStation.PCP.dll:Zone.Identifier
-echo. > PeerCastStation.TS.dll:Zone.Identifier
-echo. > PeerCastStation.UI.HTTP.dll:Zone.Identifier
-echo. > PeerCastStation.WPF.dll:Zone.Identifier
-echo. > PeerCastStation.UI.dll:Zone.Identifier
+for %%i in (*.exe,*.dll) do (
+echo. > %%i:Zone.Identifier
+)
 pause
 
