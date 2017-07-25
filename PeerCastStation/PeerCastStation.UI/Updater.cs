b870de74529bfc5b02bcb8f19dd37c8e8686d952
kumaryu <kumaryu@kumaryu.net>
2017-07-25 21:50:41 +0900

    ZIPを展開して置き換えるメソッドを追加した
    置き換えアップデート用にZIPファイルを展開して現在のファイルを置き換えるメソッドを追加した。
    ZIP内に無いファイルは削除するが、ZIP内に無いディレクトリを削除するのはちょっと実装が面倒だったので今のところ削除しない。

--- PeerCastStation/PeerCastStation.UI/Updater.cs
+++ PeerCastStation/PeerCastStation.UI/Updater.cs
@@ -85,6 +85,76 @@ namespace PeerCastStation.UI
       });
     }
 
+    private static IEnumerable<string> Glob(string path)
+    {
+      try {
+        return
+          System.IO.Directory.GetFiles(path)
+          .Concat(
+            System.IO.Directory.GetDirectories(path).SelectMany(subpath => Glob(subpath)))
+          .Select(subpath => System.IO.Path.GetFullPath(subpath));
+      }
+      catch (Exception) {
+        return Enumerable.Empty<string>();
+      }
+    }
+
+    public static void InplaceUpdate(string destpath, string filename, string[] excludes)
+    {
+      destpath = System.IO.Path.GetFullPath(destpath);
+      using (var file = System.IO.File.OpenRead(filename))
+      using (var archive = new System.IO.Compression.ZipArchive(file, System.IO.Compression.ZipArchiveMode.Read)) {
+        var entries = archive.Entries.OrderBy(ent => ent.FullName);
+        var root = entries.First();
+        string rootpath = "";
+        if (root.FullName.EndsWith("/") &&
+            entries.All(ent => ent.FullName.StartsWith(root.FullName))) {
+          rootpath = root.FullName;
+        }
+        foreach (var ent in entries) {
+          var path = System.IO.Path.Combine(destpath, ent.FullName.Substring(rootpath.Length).Replace('/', '\\'));
+          if (ent.FullName.EndsWith("/")) {
+            var info = System.IO.Directory.CreateDirectory(path);
+            try {
+              info.LastWriteTime = ent.LastWriteTime.DateTime;
+            }
+            catch (System.IO.IOException) {
+            }
+          }
+          else {
+            try {
+              using (var dst = System.IO.File.OpenWrite(path))
+              using (var src = ent.Open()) {
+                src.CopyTo(dst);
+              }
+              var info = new System.IO.FileInfo(path);
+              try {
+                info.LastWriteTime = ent.LastWriteTime.DateTime;
+              }
+              catch (System.IO.IOException) {
+              }
+            }
+            catch (System.IO.IOException) {
+              if (!excludes.Contains(ent.Name)) throw;
+            }
+          }
+        }
+        var oldentries = Glob(destpath).ToArray();
+        var newentries = entries
+          .Select(ent => ent.FullName)
+          .Where(ent => !ent.EndsWith("/"))
+          .Select(ent => System.IO.Path.Combine(destpath, ent.Substring(rootpath.Length).Replace('/', '\\'))) 
+          .ToArray();
+        foreach (var old in oldentries.Except(newentries)) {
+          try {
+            System.IO.File.Delete(old);
+          }
+          catch (System.IO.IOException) {
+          }
+        }
+      }
+    }
+
     public event NewVersionFoundEventHandler NewVersionFound;
   }
 
