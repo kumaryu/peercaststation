2b0492c610809a811898979022ec786fb0cb1031
kumaryu <kumaryu@kumaryu.net>
2018-05-10 01:27:46 +0900

    Bumped version up to 2.5.1

--- PeerCastStation/PeerCastStation.HTTP/Properties/AssemblyInfo.cs
+++ PeerCastStation/PeerCastStation.HTTP/Properties/AssemblyInfo.cs
@@ -48,6 +48,6 @@ using System.Runtime.InteropServices;
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("2.5.0")]
-[assembly: AssemblyInformationalVersion("2.5.0")]
+[assembly: AssemblyFileVersion("2.5.1.0")]
+[assembly: AssemblyInformationalVersion("2.5.1.0")]
 
