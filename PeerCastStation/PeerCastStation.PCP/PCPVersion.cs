89ae189344b5b8d5e7b6bb872a9aab872085be6c
kumaryu <kumaryu@kumaryu.net>
2019-02-11 03:22:32 +0900

    bumped version up to 2.5.4.

--- PeerCastStation/PeerCastStation.PCP/PCPVersion.cs
+++ PeerCastStation/PeerCastStation.PCP/PCPVersion.cs
@@ -9,7 +9,7 @@ namespace PeerCastStation.PCP
     public static readonly int    ServantVersion         = 1218;
     public static readonly int    ServantVersionVP       = 27;
     public static readonly byte[] ServantVersionEXPrefix = new byte[] { (byte)'S', (byte)'T' };
-    public static readonly short  ServantVersionEXNumber = 253;
+    public static readonly short  ServantVersionEXNumber = 254;
     public static readonly int    ProtocolVersionIPv4    = 1;
     public static readonly int    ProtocolVersionIPv6    = 100;
 
