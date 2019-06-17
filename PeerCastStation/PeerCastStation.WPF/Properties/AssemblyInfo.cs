2332c2224ac4d451072b78842cbb0f9875eefc14
kumaryu <kumaryu@kumaryu.net>
2019-06-17 10:46:21 +0900

    日付間違っていたので2.7.2.1として再リリースした

--- PeerCastStation/PeerCastStation.WPF/Properties/AssemblyInfo.cs
+++ PeerCastStation/PeerCastStation.WPF/Properties/AssemblyInfo.cs
@@ -67,6 +67,6 @@ using System.Windows;
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("2.7.2.0")]
-[assembly: AssemblyInformationalVersion("2.7.2.0")]
+[assembly: AssemblyFileVersion("2.7.2.1")]
+[assembly: AssemblyInformationalVersion("2.7.2.1")]
 
