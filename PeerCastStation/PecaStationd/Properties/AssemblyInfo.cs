ed1ab515f5dbc91b0f6abda532f8ae79cc464252
kumaryu <kumaryu@kumaryu.net>
2020-02-25 23:49:53 +0900

    bumped version up to 2.9.3

--- PeerCastStation/PecaStationd/Properties/AssemblyInfo.cs
+++ PeerCastStation/PecaStationd/Properties/AssemblyInfo.cs
@@ -33,4 +33,4 @@ using System.Runtime.InteropServices;
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.9.1.0")]
-[assembly: AssemblyFileVersion("2.9.2.0")]
+[assembly: AssemblyFileVersion("2.9.3.0")]
--- PeerCastStation/PeerCastStation.CustomFilter/Properties/AssemblyInfo.cs
+++ PeerCastStation/PeerCastStation.CustomFilter/Properties/AssemblyInfo.cs
@@ -33,4 +33,4 @@ using System.Runtime.InteropServices;
 // 以下のように '*' を使用します:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("2.9.2.0")]
+[assembly: AssemblyFileVersion("2.9.3.0")]
