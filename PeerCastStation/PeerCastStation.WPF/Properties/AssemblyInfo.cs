ca4d6fe5ea6ff02e18d20c1be4e73256e0030cad
kumaryu <kumaryu@kumaryu.net>
2020-12-16 01:04:00 +0900

    bumped version up to 2.9.4

--- PeerCastStation/PeerCastStation.WPF/Properties/AssemblyInfo.cs
+++ PeerCastStation/PeerCastStation.WPF/Properties/AssemblyInfo.cs
@@ -67,6 +67,6 @@ using System.Windows;
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("2.9.3.0")]
-[assembly: AssemblyInformationalVersion("2.9.3.0")]
+[assembly: AssemblyFileVersion("2.9.4.0")]
+[assembly: AssemblyInformationalVersion("2.9.4.0")]
 
