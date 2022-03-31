0761507b75bb7ff6e0aaefd6fcf3b8ecdc805962
kumaryu <kumaryu@kumaryu.net>
2022-03-31 22:26:28 +0900

    bumped version up to 2.9.8.

--- PeerCastStation/PeerCastStation.WPF/Properties/AssemblyInfo.cs
+++ PeerCastStation/PeerCastStation.WPF/Properties/AssemblyInfo.cs
@@ -67,6 +67,6 @@ using System.Windows;
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("2.9.7.0")]
-[assembly: AssemblyInformationalVersion("2.9.7.0")]
+[assembly: AssemblyFileVersion("2.9.8.0")]
+[assembly: AssemblyInformationalVersion("2.9.8.0")]
 
