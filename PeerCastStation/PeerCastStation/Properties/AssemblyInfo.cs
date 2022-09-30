35363d8135d1def494de350effb964d6c1a26224
kumaryu <kumaryu@kumaryu.net>
2022-09-30 22:11:30 +0900

    Bumped version up to 5.1.0.

--- PeerCastStation/PeerCastStation/Properties/AssemblyInfo.cs
+++ PeerCastStation/PeerCastStation/Properties/AssemblyInfo.cs
@@ -33,5 +33,5 @@ using System.Runtime.InteropServices;
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("5.0.0.0")]
-[assembly: AssemblyInformationalVersion("5.0.0.0")]
+[assembly: AssemblyFileVersion("5.1.0.0")]
+[assembly: AssemblyInformationalVersion("5.1.0.0")]
