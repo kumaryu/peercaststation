a9881a765c2d0a388658cdc00b3701f1554498c2
kumaryu <kumaryu@kumaryu.net>
2022-10-07 00:06:45 +0900

    Bumped version up to 4.1.0.

--- PeerCastStation/PeerCastStation.ASF/Properties/AssemblyInfo.cs
+++ PeerCastStation/PeerCastStation.ASF/Properties/AssemblyInfo.cs
@@ -33,5 +33,5 @@ using System.Runtime.InteropServices;
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("4.0.0.0")]
-[assembly: AssemblyInformationalVersion("4.0.0.0")]
+[assembly: AssemblyFileVersion("4.1.0.0")]
+[assembly: AssemblyInformationalVersion("4.1.0.0")]
