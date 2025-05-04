2bbbe464fdefd14a4085ddfaf14f6b8c414156ae
kumaryu <kumaryu@kumaryu.net>
2025-05-04 22:04:05 +0900

    bumped version up to 6.0.0.

--- PeerCastStation/PeerCastStation.CustomFilter/Properties/AssemblyInfo.cs
+++ PeerCastStation/PeerCastStation.CustomFilter/Properties/AssemblyInfo.cs
@@ -33,4 +33,4 @@ using System.Runtime.InteropServices;
 // 以下のように '*' を使用します:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("7.1.0.0")]
+[assembly: AssemblyFileVersion("6.0.0.0")]
