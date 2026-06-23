b80ba8e7c84ddcdd3fc2b4de5089d90851a4109f
philmist <philmist.j@gmail.com>
2026-06-23 17:19:27 +0900

    chore: SPDX-License-Identifierを新規作成したソースに付記

--- PeerCastStation/PeerCastStation.Test/MKVTests.fs
+++ PeerCastStation/PeerCastStation.Test/MKVTests.fs
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// (C) 2026 Philmist
 module MKVTests
 
 open System.IO
--- PeerCastStation/PeerCastStation.Test/MatroskaContentBufferTests.fs
+++ PeerCastStation/PeerCastStation.Test/MatroskaContentBufferTests.fs
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// (C) 2026 Philmist
 module MatroskaContentBufferTests
 
 open System
