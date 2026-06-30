b44e80f1d9ec9a0358ef6635b6fb5b5b0ebdc5b0
philmist <philmist.j@gmail.com>
2026-06-30 16:04:45 +0900

    著作者表記をSPDX形式で追加

--- PeerCastStation/PeerCastStation.FLV/FLVToMKV.cs
+++ PeerCastStation/PeerCastStation.FLV/FLVToMKV.cs
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Copyright (C) 2026 Philmist
 using PeerCastStation.Core;
 using PeerCastStation.FLV.AMF;
 using PeerCastStation.FLV.RTMP;
--- PeerCastStation/PeerCastStation.Test/FLVToMKVTests.fs
+++ PeerCastStation/PeerCastStation.Test/FLVToMKVTests.fs
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Copyright (C) 2026 Philmist
 module FLVToMKVTests
 
 open Xunit
